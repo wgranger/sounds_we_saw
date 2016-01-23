aa95b4ed5c12df3c52df936431c542de47bc01dc
wgranger <twgranger@gmail.com>
2016-01-23 16:30:50 -0600

    Add Rails Project

---
+++ db/migrate/20160123220811_create_articles_table.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.references :user
+      t.references :song
+      t.timestamps
+    end
+  end
+end
