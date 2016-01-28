63d1111f5532538c2b8295e4b296595ee2607cd4
wgranger <twgranger@gmail.com>
2016-01-28 14:13:26 -0600

    Secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, '64295f68bbe24ac7ac3154f6d6a1ee5c', '4255bfa20eae402590843b749a158570', scope: 'playlist-modify-public playlist-modify-private'
+  provider :spotify, ENV["SPOTIFY_CLIENT"], ENV["SPOTIFY_SECRET"], scope: 'playlist-modify-public playlist-modify-private'
 end
